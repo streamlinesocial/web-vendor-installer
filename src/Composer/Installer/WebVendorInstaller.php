cd99c540f46b59e671ace5b2c59667feae322125
Chris <chris@buryta.com>
2012-12-15 22:59:21 -0500

    initial create of the installer file

---
+++ src/Composer/Installer/WebVendorInstaller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace streamlinesocial\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class TemplateInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        return implode(DIRECTORY_SEPARATOR, array('web', 'vendor', $package->getPrettyName()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'web-vendor' === $packageType;
+    }
+}
